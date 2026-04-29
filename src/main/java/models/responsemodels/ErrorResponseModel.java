1ff444b201745e7459c7ca4be7df0fb585a3549a
Mohamed Khedr <mkhedr652@gmail.com>
2026-04-29 21:01:13 +0300

    Create Error Response Model

--- src/main/java/models/responsemodels/ErrorResponseModel.java
+++ src/main/java/models/responsemodels/ErrorResponseModel.java
@@ -1,4 +1,26 @@
 package models.responsemodels;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "path",
+        "timestamp",
+        "name",
+        "message"
+})
+@Generated("jsonschema2pojo")
 public class ErrorResponseModel {
+
+    @JsonProperty("path")
+    public String path;
+    @JsonProperty("timestamp")
+    public String timestamp;
+    @JsonProperty("name")
+    public String name;
+    @JsonProperty("message")
+    public String message;
+
 }
