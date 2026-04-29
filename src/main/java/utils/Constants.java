789bb5683752f2612292cf48fbeb6c2317187c53
Mohamed Khedr <mkhedr652@gmail.com>
2026-04-29 21:24:49 +0300

    Enhancement for static attributes

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class Constants {
-    static String baseUrl = "https://api.escuelajs.co/api/v1";
-    static String CATEGORY_END_POINT = "categories";
-    static String PRODUCT_END_POINT = "products";
+     public static final String baseUrl = "https://api.escuelajs.co/api/v1";
+    public static final String CATEGORY_END_POINT = "categories";
+    public static final String PRODUCT_END_POINT = "products";
 }
