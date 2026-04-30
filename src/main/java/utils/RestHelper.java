336998a435e06250f60410ca169ea68d38e3267d
Mohamed Khedr <mkhedr652@gmail.com>
2026-04-30 21:07:09 +0300

    Enhancement for the api requests using templates

--- src/main/java/utils/RestHelper.java
+++ src/main/java/utils/RestHelper.java
@@ -1,5 +1,6 @@
 package utils;
 import io.restassured.common.mapper.TypeRef;
+import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class RestHelper {
@@ -32,6 +33,30 @@ public class RestHelper {
                 .then().statusCode(expectedStatusCode).log().all()
                 .extract()
                 .as(typeRef);
+    }
+
+    public static <T> T restPost(String URL, String endpoint, Object requestBody, Class<T> responseClass, int expectedStatusCode) {
+        return given()
+                .relaxedHTTPSValidation()
+                .contentType("application/json")
+                .body(requestBody)
+                .when().log().all()
+                .post(URL.concat(endpoint))
+                .then().statusCode(expectedStatusCode).log().all()
+                .extract()
+                .as(responseClass);
+    }
+
+    public static <T> T restGet(String URL, String endpoint, Map<String, String> headers, Class<T> responseClass, int expectedStatusCode) {
+        return given()
+                .relaxedHTTPSValidation()
+                .contentType("application/json")
+                .headers(headers)
+                .when().log().all()
+                .get(URL.concat(endpoint))
+                .then().statusCode(expectedStatusCode).log().all()
+                .extract()
+                .as(responseClass);
     }
 
 }
