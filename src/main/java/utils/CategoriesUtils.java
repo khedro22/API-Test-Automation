1ae18ecd6e471ff87bfd4dc89b9bcc2978e4f17e
Mohamed Khedr <mkhedr652@gmail.com>
2026-04-30 21:51:20 +0300

    Adding create methods

--- src/main/java/utils/CategoriesUtils.java
+++ src/main/java/utils/CategoriesUtils.java
@@ -2,6 +2,9 @@ package utils;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import models.requestmodels.CreateProductRequest;
+import models.responsemodels.ErrorResponseModel;
+import models.responsemodels.SingleCategory;
 
 public class CategoriesUtils {
     public static Response getAllCategories(){
@@ -14,6 +17,20 @@ public class CategoriesUtils {
         return RestAssured.given().log().all().
                 get(Constants.BASE_URL + Constants.CATEGORY_END_POINT + "/" + id).
                 then().extract().response();
+    }
+
+    public static SingleCategory getCATegory(int expectedStatusCode, int id){
+        return RestHelper.restGet(Constants.BASE_URL, Constants.CATEGORY_END_POINT, "/"+id, SingleCategory.class, expectedStatusCode);
+    }
+    public static ErrorResponseModel getInvalidCategory(int expectedStatusCode, int id){
+        return RestHelper.restGet(Constants.BASE_URL, Constants.CATEGORY_END_POINT, "/"+id, ErrorResponseModel.class, expectedStatusCode);
+    }
+    public static SingleCategory createCategory(CreateProductRequest requestBody, int expectedStatusCode){
+        return RestHelper.restPost(Constants.BASE_URL, Constants.CATEGORY_END_POINT, requestBody, SingleCategory.class, expectedStatusCode);
+    }
+    public static ErrorResponseModel createInvalidCategory(CreateProductRequest requestBody, int expectedStatusCode){
+        return RestHelper.restPost(Constants.BASE_URL, Constants.CATEGORY_END_POINT, requestBody, ErrorResponseModel.class, expectedStatusCode
+        );
     }
 
 }
